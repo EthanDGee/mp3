a7054f714e6dab23832922c366b9a9d3b84d5f36
EthanDGee <workethangee@gmail.com>
2026-08-27 13:04:43 -0600

    feat(player): added get_artists()
    This returns a list of all artists available on mpd.

--- src/player.py
+++ src/player.py
@@ -8,7 +8,8 @@ class MP3:
         self.client: MPDClient = MPDClient()
         self.client.timeout = 10
         self.client.connect("localhost", CONNECTION_PORT)
-        print(self.client.list("artist"))
+        print(self.get_artists())
+        print(self.client.list("album"))
 
     def toggle_play(self):
         status = self.client.status()
@@ -16,6 +17,14 @@ class MP3:
             self.client.pause(1)
         else:
             self.client.play()
+
+    def get_artists(self) -> list[str]:
+        artist_json = self.client.list("artist")
+        artists = []
+        for i in artist_json:
+            artists.append(i["artist"])
+
+        return artists
 
 
 if __name__ == "__main__":
